5da532f1907f74803e2830c2a62212b35a48d038
Divyanshu115 <divyanshu0069.becse24@chitkara.edu.in>
2025-02-05 10:40:29 +0530

    Solution to Logical AND and OR Combination

--- Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-int main() {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    bool condition (a>0 && b<0) ||(a==0 && b==0);
-    if(condition){
-        printf("True\n");
+#include <stdbool.h>
 
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    bool condition = (a > 0 && b < 0) || (a == 0 && b == 0);
+
+    if (condition) {
+        printf("True\n");
+    } else {
+        printf("False\n");
     }
-    else{
-        printf("Falase\n");
-    }
+
     return 0;
-    }
-    
+}
